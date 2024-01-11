8158ad04a1cdc69ea972291b60689dd63d2320c4
yvonne <yvonnewong1107@gmail.com>
2024-01-11 19:13:48 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CharacterGallery from './components/CharacterGallery';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="site-title">A Song of Fire and Ice</h1>
+      <CharacterGallery />
+    </>
+  );
+}
+
+export default App
